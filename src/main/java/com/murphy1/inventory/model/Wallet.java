4e571050b644a55358f595c98ca8314007e6f479
Stephen Murphy <smurphy54321@gmail.com>
2019-08-15 17:23:01 -0400

    Created the User and Wallet POJO's. Closes #7

---
+++ src/main/java/com/murphy1/inventory/model/Wallet.java
@@ -0,0 +1,23 @@
+package com.murphy1.inventory.model;
+
+public class Wallet {
+
+    private Long id;
+    private Double balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
