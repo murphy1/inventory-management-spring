03a822d15f03b3b622ed3ea830f96702c6d05688
Stephen Murphy <smurphy54321@gmail.com>
2019-08-27 22:15:40 -0400

    Can now create a user with the userform page. Need to create tests for the controller and service layer. #26

--- src/main/java/com/murphy1/inventory/controllers/UserController.java
+++ src/main/java/com/murphy1/inventory/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.murphy1.inventory.controllers;
 
+import com.murphy1.inventory.model.User;
 import com.murphy1.inventory.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +23,20 @@ public class UserController {
         model.addAttribute("users", userService.getAllUsers());
 
         return "user";
+    }
+
+    @RequestMapping("user/new")
+    public String newUser(Model model){
+        model.addAttribute("user", new User());
+
+        return "forms/userform";
+    }
+
+    @PostMapping("user/new/user")
+    public String saveAndUpdate(@ModelAttribute User user){
+        User savedUser = userService.saveUser(user);
+
+        return "redirect:/users.html";
     }
 
 }
--- src/main/java/com/murphy1/inventory/services/impl/UserServiceImpl.java
+++ src/main/java/com/murphy1/inventory/services/impl/UserServiceImpl.java
@@ -3,11 +3,15 @@ package com.murphy1.inventory.services.impl;
 import com.murphy1.inventory.model.User;
 import com.murphy1.inventory.repositories.UserRepository;
 import com.murphy1.inventory.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -22,5 +26,14 @@ public class UserServiceImpl implements UserService {
         List<User> users = new ArrayList<>();
         userRepository.findAll().forEach(users::add);
         return users;
+    }
+
+    @Transactional
+    @Override
+    public User saveUser(User user) {
+
+        userRepository.save(user);
+
+        return user;
     }
 }
